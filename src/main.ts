b4f590e2b8d0bf6591f2e9a25a8eb63aeb635515
Adiel <adielp16@gmail.com>
2022-08-04 10:45:18 -0300

    :card_file_box: config prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@modules/app.module';
+import { PrismaService } from '@services/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
 }
 bootstrap();
